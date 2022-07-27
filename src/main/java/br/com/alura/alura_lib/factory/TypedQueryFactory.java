57afcf8099ae44c0dead49f58b6282eb82a7be0a
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-07-26 22:03:31 -0300

    Aula 03 lição 4 - qualificadores

---
+++ src/main/java/br/com/alura/alura_lib/factory/TypedQueryFactory.java
@@ -0,0 +1,29 @@
+package br.com.alura.alura_lib.factory;
+
+import java.lang.reflect.ParameterizedType;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.alura.alura_lib.jpa.annotation.Query;
+
+@SuppressWarnings("unchecked")
+public class TypedQueryFactory {
+	
+	@Inject
+	private EntityManager em;
+	
+	@Produces
+	@Query("")
+	public <X> TypedQuery<X > factory( InjectionPoint point ) {
+		ParameterizedType type = (ParameterizedType) point.getType();
+		
+		Class<X> classe = (Class<X>) type.getActualTypeArguments()[0];
+		String jpql = point.getAnnotated().getAnnotation(Query.class).value();
+		return em.createQuery(jpql, classe);
+	}
+
+}
